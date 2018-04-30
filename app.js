f7c6fe7b02ccf42d9b500644806a5143725ef117
Jon Jelinski <jon_jelinski@t-online.de>
2018-04-30 11:37:54 +0300

    Fix express request id usage

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const expressRequestId = require('express-request-id');
+const expressRequestId = require('express-request-id')();
 
 const app = express();
 
-//app.use(expressRequestId);
+app.use(expressRequestId);
 
 const port = 3075;
 
 app.get('/', function (req, res) {
+
+  console.log('request id: ' + req.id);
   res.send('Pong!');
 })
 
