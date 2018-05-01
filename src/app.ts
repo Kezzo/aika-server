1ab847e6de78f7b92da0084ee0217da2f82aff2e
Jon Jelinski <jon_jelinski@t-online.de>
2018-05-01 15:17:11 +0300

    Implement basic dynamodb access

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,32 @@
-const express = require('express');
-const expressRequestId = require('express-request-id')();
+import { Router, Request, Response } from 'express';
+
+import express = require('express');
+import expressRequestId = require('express-request-id');
+import AWS = require('aws-sdk');
+
+AWS.config.update({
+  region: "eu-west-1"/*,
+  accessKeyId: process.env.DYNAMODB_ACCESS_KEY_ID,
+  secretAccessKey : process.env.DYNAMODB_SECRET_ACCESS_KEY*/
+});
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 const app = express();
 
-app.use(expressRequestId);
+app.use(expressRequestId());
 
 const port = 3075;
 
-app.get('/', function (req, res) {
+app.get('/', function (req:Request, res: Response) {
 
-  console.log('request id: ' + req.id);
-  res.send('Pong!');
+  dynamodb.get({
+    TableName: "users",
+    Key: { "ID": "12345" }
+  }, function(error, data){
+    res.send('Pong! Loaded data: ' + JSON.stringify(data));
+  });
+  //console.log('request id: ' + req.id);
 })
 
 app.listen(port);
