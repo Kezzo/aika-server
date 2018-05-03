4d6ce9237b2de629f57ca0771e547b6be6a03647
Jon Jelinski <jon_jelinski@t-online.de>
2018-05-03 23:12:48 +0300

    Added create, update and delete database access methods

--- src/db-access.ts
+++ src/db-access.ts
@@ -17,14 +17,63 @@ export class DatabaseAccess {
         console.log('DatabaseAccess Init!');
     }
 
+    public static async Create(params)
+    {
+        console.log('DB Create: ' + JSON.stringify(params));
+
+        const dynamodb = this.dynamodb;
+        return new Promise(function(resolve, reject) {
+            dynamodb.put(params, function(error, data) {
+                console.log('DB Created data:' + JSON.stringify(data) + ' error: ' + error);
+                if(!_.isNull(error)) {
+                    return reject(error);
+                }
+
+                return resolve(data);
+            });
+        });
+    }
+
     public static async Get(params)
     {
         console.log('DB Get: ' + JSON.stringify(params));
-        console.log('dynamodb undefined? ' + _.isUndefined(this.dynamodb));
         const dynamodb = this.dynamodb;
         return new Promise(function(resolve, reject) {
             dynamodb.get(params, function(error, data) {
                 console.log('DB Got data:' + JSON.stringify(data) + ' error: ' + error);
+                if(!_.isNull(error)) {
+                    return reject(error);
+                }
+
+                return resolve(data);
+            });
+        });
+    }
+
+    public static async Update(params)
+    {
+        console.log('DB Update: ' + JSON.stringify(params));
+        const dynamodb = this.dynamodb;
+        return new Promise(function(resolve, reject) {
+            dynamodb.update(params, function(error, data) {
+                console.log('DB Updated data:' + JSON.stringify(data) + ' error: ' + error);
+                if(!_.isNull(error)) {
+                    return reject(error);
+                }
+
+                return resolve(data);
+            });
+        });
+    }
+
+    public static async Delete(params)
+    {
+        console.log('DB Delete: ' + JSON.stringify(params));
+
+        const dynamodb = this.dynamodb;
+        return new Promise(function(resolve, reject) {
+            dynamodb.delete(params, function(error, data) {
+                console.log('DB Deleted data:' + JSON.stringify(data) + ' error: ' + error);
                 if(!_.isNull(error)) {
                     return reject(error);
                 }
