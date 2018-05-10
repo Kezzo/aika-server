664d2b989a60debd20d4ed0c5ee146edb97b50e1
Jon Jelinski <jon_jelinski@t-online.de>
2018-05-11 00:13:26 +0300

    Update user query to use new table and index names

--- src/queries/user-query.ts
+++ src/queries/user-query.ts
@@ -5,8 +5,8 @@ export class UserQuery {
   public static async GetUser(logger: AppLogger, userId) {
     logger.Info('GetUser for id: ' + userId);
     const userData = await DatabaseAccess.Get(logger, {
-      TableName: 'users',
-      Key: { ID: userId }
+      TableName: 'USERS',
+      Key: { UID: userId }
     });
 
     logger.Info('Got data for id: ' + userId + ': ' + JSON.stringify(userData));
